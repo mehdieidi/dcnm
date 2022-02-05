81f75363f1c1ede21f36f3d0a1b4ede4442f1044
MehdiEidi <mehdiadq@gmail.com>
2022-02-05 12:09:13 +0330

    map made thread-safe

--- main.go
+++ main.go
@@ -5,24 +5,33 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/mux"
 )
 
-var store = make(map[string]string)
+var store = struct {
+	sync.RWMutex
+	m map[string]string
+}{m: make(map[string]string)}
 
 var (
 	ErrorNoSuchKey = errors.New("no such key")
 )
 
 func Put(key string, value string) error {
-	store[key] = value
+	store.Lock()
+	store.m[key] = value
+	store.Unlock()
 
 	return nil
 }
 
 func Get(key string) (string, error) {
-	value, ok := store[key]
+	store.RLock()
+	value, ok := store.m[key]
+	store.RUnlock()
+
 	if !ok {
 		return "", ErrorNoSuchKey
 	}
@@ -31,7 +40,10 @@ func Get(key string) (string, error) {
 }
 
 func Delete(key string) error {
-	delete(store, key)
+	store.Lock()
+	delete(store.m, key)
+	store.Unlock()
+
 	return nil
 }
 
